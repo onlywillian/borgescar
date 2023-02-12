f1faa9b10825ce6491448a12fc14f59eb7e07e26
onlywillian <willianandrade953@gmail.com>
2023-02-12 11:10:07 -0300

    menu ref

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link"
+import { useRef } from "react"
 
 export default function Header() {
+    let menuRef = useRef<HTMLDivElement | null>(null);
+
+    function handleMenuClick() {
+        // menu action
+    }
+
     return (
         <div className="p-4 flex w-full justify-between">
             <h1 className='text-3xl'>BORGE'S CAR</h1>
@@ -8,7 +15,7 @@ export default function Header() {
             <div className='flex'>
                 <Link href={'/conta/login'} className='mr-6'>LOGIN ||| CADASTRO</Link>
 
-                <div className='cursor-pointer'>
+                <div className='cursor-pointer' onClick={handleMenuClick} ref={menuRef}>
                     <div className='w-10 bg-black h-1 my-1'></div>
                     <div className='w-10 bg-black h-1 my-1'></div>
                     <div className='w-10 bg-black h-1 my-1'></div>
