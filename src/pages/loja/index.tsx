95cde5c72d47d663a025d1abbddb46790db6286d
onlywillian <willianandrade953@gmail.com>
2023-03-19 09:46:20 -0300

    store modifications

--- src/pages/loja/index.tsx
+++ src/pages/loja/index.tsx
@@ -1,18 +1,23 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import logoImage from '../../../public/logo-light.svg'
+
 export default function Loja() {
     return (
         <>
         <Head>
             <title>Loja</title>
         </Head>
-        <main className="h-screen bg-aqua-v-light">
-            <aside className="w-1/5 bg-aside-bg h-screen fixed">
-
+        <main className="h-screen bg-aqua-v-light flex">
+            <aside className="w-1/5 bg-aside-bg h-screen sticky flex flex-col">
+                <Image src={logoImage} alt='logo' className="h-28"/>
             </aside>
-            <div>
-
+            <div className="px-10 w-full">
+                <div className="bg-gray-500 w-full h-2/5">Landing</div>
+                <div>
+                    
+                </div>
             </div>
         </main>
         </>
