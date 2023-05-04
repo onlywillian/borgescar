71f6dd76e0cdd5bbe49248badccea99160a96a21
onlywillian <willianandrade953@gmail.com>
2023-05-04 11:26:34 -0300

    carousel work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,44 +10,62 @@ import carImage from "../../public/assets/cars/aston.jpg";
 import { useState } from "react";
 
 export default function Home() {
-  const [images, setImages] = useState([]);
+  const [index, setindex] = useState([0, 1, 2]);
+  const [images, setImages] = useState([
+    {
+      link: 'https://s1.1zoom.me/b5050/112/Lamborghini_Gran_Turismo_Roads_Veneno_Back_view_567887_1920x1080.jpg',
+    },
+    {
+      link: 'https://s1.1zoom.me/b4954/870/Lamborghini_VAG_Performante_Huracan_Orange_567517_1920x1080.jpg',
+    },
+    {
+      link: 'https://wallpapercave.com/wp/wp4429281.jpg',
+    },
+  ]);
+
+  function handleImagesClick(way: string) {
+    if (way === 'next') {
+
+    }
+  }
 
   return (
     <>
       <main className="h-screen overflow-auto">
         <Header />
         <div className="w-full overflow-auto">
-          <div className="flex h-full transition-transform duration-300 overflow-hidden gap-10 relative justify-center">
-            <div className="w-2/3 absolute flex-shrink-0 -left-1/2 opacity-60 cursor-pointer hover:opacity-100 transition">
+          <div className="flex h-screen overflow-hidden relative justify-center">
+            <div 
+              className="w-2/3 h-5/6 absolute -left-1/2 opacity-60 cursor-pointer hover:opacity-100 transition self-end"
+              onClick={() => handleImagesClick('previous')}
+            >
               <Image
-                src={
-                  "https://s1.1zoom.me/b5050/112/Lamborghini_Gran_Turismo_Roads_Veneno_Back_view_567887_1920x1080.jpg"
-                }
-                alt="sadsad"
+                src={images[index[0]].link}
+                alt="Carro"
                 width={1920}
                 height={1080}
                 className="w-full h-full"
               />
             </div>
-            <div className="w-2/3 flex-shrink-0">
+            <div className="w-2/3 h-5/6 self-start">
               <Image
-                src={
-                  "https://s1.1zoom.me/b4954/870/Lamborghini_VAG_Performante_Huracan_Orange_567517_1920x1080.jpg"
-                }
-                alt="sadsad"
+                src={images[index[1]].link}
+                alt="Carro"
                 width={1920}
-                height={1080}
+                height={0}
                 className="w-full h-full"
               />
             </div>
-            <div className="w-2/3 absolute flex-shrink-0 -right-1/2 opacity-60 cursor-pointer hover:opacity-100 transition">
+            <div 
+              className="w-2/3 h-5/6 absolute -right-1/2 opacity-60 cursor-pointer hover:opacity-100 transition self-end" 
+              onClick={() => handleImagesClick('next')}
+            >
               <Image
-                src={"https://wallpapercave.com/wp/wp4429281.jpg"}
-                alt="sadsad"
+                src={images[index[2]].link}
+                alt="Carro"
                 width={1920}
                 height={1080}
                 className="w-full h-full"
-                loading="lazy"
               />
             </div>
           </div>
