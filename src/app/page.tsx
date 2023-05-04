2c4eb35f62269cb873ec6c532d10149e1467e4f2
onlywillian <willianandrade953@gmail.com>
2023-05-04 00:15:04 -0300

    carousel start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,37 +6,23 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Logos from "@/components/Logos";
 
-import { useRef } from "react";
+import carImage from "../../public/assets/cars/aston.jpg";
 
 export default function Home() {
-  const carouselRef = useRef<HTMLDivElement>(null);
-
-  const scrollToSlide = (slideIndex: number) => {
-    const slide = carouselRef.current?.children[slideIndex];
-    slide?.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
-  };
-
   return (
     <>
       <main className="h-screen overflow-auto">
         <Header />
         <div className="w-full overflow-auto">
-          <div
-            className="flex transition-transform duration-300"
-            ref={carouselRef}
-          >
-            <div className="w-2/3 flex-shrink-0">
+          <div className="flex h-full transition-transform duration-300 overflow-hidden gap-10 relative justify-center">
+            <div className="w-2/3 absolute flex-shrink-0 -left-1/2 opacity-60 cursor-pointer hover:opacity-100 transition">
               <Image
                 src={
-                  "https://s1.1zoom.me/b4954/870/Lamborghini_VAG_Performante_Huracan_Orange_567517_1920x1080.jpg"
+                  "https://s1.1zoom.me/b5050/112/Lamborghini_Gran_Turismo_Roads_Veneno_Back_view_567887_1920x1080.jpg"
                 }
                 alt="sadsad"
-                width={10000}
-                height={10000}
+                width={1920}
+                height={1080}
                 className="w-full h-full"
               />
             </div>
@@ -46,45 +32,43 @@ export default function Home() {
                   "https://s1.1zoom.me/b4954/870/Lamborghini_VAG_Performante_Huracan_Orange_567517_1920x1080.jpg"
                 }
                 alt="sadsad"
-                width={10000}
-                height={10000}
+                width={1920}
+                height={1080}
                 className="w-full h-full"
               />
             </div>
-            <div className="w-2/3 flex-shrink-0">
+            <div className="w-2/3 absolute flex-shrink-0 -right-1/2 opacity-60 cursor-pointer hover:opacity-100 transition">
               <Image
-                src={
-                  "https://s1.1zoom.me/b4954/870/Lamborghini_VAG_Performante_Huracan_Orange_567517_1920x1080.jpg"
-                }
+                src={"https://wallpapercave.com/wp/wp4429281.jpg"}
                 alt="sadsad"
-                width={10000}
-                height={10000}
+                width={1920}
+                height={1080}
                 className="w-full h-full"
+                loading="lazy"
               />
             </div>
-          </div>
-
-          <div className="flex w-full justify-center mt-4">
-            <button
-              className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
-              onClick={() => scrollToSlide(0)}
-            />
-            <button
-              className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
-              onClick={() => scrollToSlide(1)}
-            />
-            <button
-              className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
-              onClick={() => scrollToSlide(2)}
-            />
           </div>
         </div>
-        {/* <div className="w-full my-10 flex justify-center gap-10">
+        {/* <div className="flex w-full justify-center mt-4">
+          <button
+            className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
+            onClick={() => scrollToSlide(0)}
+          />
+          <button
+            className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
+            onClick={() => scrollToSlide(1)}
+          />
+          <button
+            className="mx-2 w-4 h-4 rounded-full bg-gray-500 transition-colors duration-300"
+            onClick={() => scrollToSlide(2)}
+          />
+        </div> */}
+        <div className="w-full my-10 flex justify-center gap-10">
           <Logos />
         </div>
         <div className="h-full bg-gray-200 w-full">
           <Image src={carImage} alt="Aston Martin" className="w-full h-full" />
-        </div> */}
+        </div>
         <Footer />
       </main>
     </>
