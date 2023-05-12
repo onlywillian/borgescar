73918763cb6e0934c4d6bc641f25c2a65655b626
onlywillian <willianandrade953@gmail.com>
2023-05-12 11:24:48 -0300

    new car layout and logic finished

--- src/app/adm/novo-carro/page.tsx
+++ src/app/adm/novo-carro/page.tsx
@@ -5,22 +5,47 @@ import Aside from "@/components/Aside";
 import Button from "@/components/Button";
 import { FormEvent, useState } from "react";
 
-export default function NewCar() {
-  // const [carName, setCarName] = useState("");
-  // const [carDescription, setCarDescription] = useState("");
-  // const [carType, setCarType] = useState("");
-  // const [carPrice, setCarPrice] = useState("");
-  // const [carStock, setCarStock] = useState("");
+type eventType = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>
 
-  const [file, setFile] = useState<File | null>(null);
+export default function NewCar() {
+  const [carData, setCarData] = useState({
+    name: "",
+    description: "",
+    type: "",
+    price: "",
+    stock: "",
+  });
+
+  
+  const [file1, setFile1] = useState<File | null>(null);
+  const [file2, setFile2] = useState<File | null>(null);
+  const [file3, setFile3] = useState<File | null>(null);
+
+  const handleInputChange = (e: eventType) => {
+    const { id, value } = e.target;
+    
+    // Inserindo com base no id
+    setCarData((prevUserData) => ({ ...prevUserData, [id]: value }));
+  };
 
   async function handleFormSubmit(e: FormEvent) {
     e.preventDefault();
 
     const formData = new FormData();
 
-    formData.append("file", file as File);
+    // inserindo as imagens
+    formData.append("image1", file1 as File);
+    formData.append("image2", file2 as File);
+    formData.append("image3", file3 as File);
 
+    // Inserindo os dados do formulário
+    formData.append("name", carData.name);
+    formData.append("description", carData.description);
+    formData.append("type", carData.type);
+    formData.append("price", carData.price);
+    formData.append("stock", carData.stock);
+
+    // Mandando a requisição
     const response = await fetch("http://localhost:8000/cars/new", {
       method: "POST",
       body: formData,
@@ -41,51 +66,71 @@ export default function NewCar() {
           <div className="flex w-full h-full justify-between">
             <div className="flex flex-col justify-center w-1/3">
               <Input
-                id="nome-veiculo"
+                id="name"
                 label="Nome do Veiculo"
                 type="text"
                 placeholder="Ex: Aston Martin Vulcan"
+                handleChange={handleInputChange}
               />
               <>
                 <label htmlFor="espec" className="font-bold ml-4">
                   Especificações
                 </label>
                 <textarea
-                  id="espec"
+                  id="description"
                   className="h-1/5 resize-none border-none bg-purple-input rounded-xl text-white p-2 outline-0"
                   placeholder="Descrição completa do veículo"
+                  onChange={(e) => handleInputChange(e)}
                 ></textarea>
               </>
               <Input
-                id="tipo-veiculo"
+                id="type"
                 label="Tipo do Veiculo"
                 type="text"
                 placeholder="Ex: Esportivo"
+                handleChange={handleInputChange}
               />
               <Input
-                id="preco-veiculo"
+                id="price"
                 label="Preço (Em Reais)"
                 type="text"
                 placeholder="Ex: 1.000.000"
+                handleChange={handleInputChange}
               />
               <Input
-                id="estoque-veiculo"
+                id="stock"
                 label="Estoque (Unidades)"
                 type="number"
                 placeholder="Ex: 10"
+                handleChange={handleInputChange}
               />
             </div>
             <div className="flex flex-col justify-center w-1/3">
+              {/* Upload de imagens */}
               <label>Imagem 1</label>
               <input
                 type="file"
-                name="img1"
+                name="image1"
                 className="bg-purple-input border-none outline-0 p-2 rounded-xl text-lg mb-5 text-white"
-                onChange={(e) => setFile(e.target.files![0])}
+                onChange={(e) => setFile1(e.target.files![0])}
+                accept="image/*"
               />
-              {/* <Input id="img1" label="Segunda Imagem do Carro" type="file" />
-              <Input id="img1" label="Terceira Imagem do Carro" type="file" /> */}
-              {/* <Input id="som" label="Som do Veículo" type="file" /> */}
+              <label>Imagem 2</label>
+              <input
+                type="file"
+                name="image1"
+                className="bg-purple-input border-none outline-0 p-2 rounded-xl text-lg mb-5 text-white"
+                onChange={(e) => setFile2(e.target.files![0])}
+                accept="image/*"
+              />
+              <label>Imagem 3</label>
+              <input
+                type="file"
+                name="image1"
+                className="bg-purple-input border-none outline-0 p-2 rounded-xl text-lg mb-5 text-white"
+                onChange={(e) => setFile3(e.target.files![0])}
+                accept="image/*"
+              />
             </div>
           </div>
           <div className="flex gap-10">
